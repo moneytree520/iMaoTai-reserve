687f7dfa521a17636f5bce4d0dea71ae22d72cef
moneytree520 <77653761+moneytree520@users.noreply.github.com>
2024-01-15 16:24:35 +0800

    Update config.py

--- config.py
+++ config.py
@@ -34,8 +34,8 @@ AMAP_KEY = os.environ.get("GAODE_KEY")
 不配置,使用默认路径,在项目目录中;如果需要配置,你自己应该也会配置路径
 例如： CREDENTIALS_PATH = './myConfig/credentials'
 '''
-#CREDENTIALS_PATH = None
-CREDENTIALS_PATH = './myConfig/credentials'
+CREDENTIALS_PATH = None
+#CREDENTIALS_PATH = './myConfig/credentials'
 
 '''
 *********** 个人加解密密钥 ***********
